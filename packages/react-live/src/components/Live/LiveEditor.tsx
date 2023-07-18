153b4a8dee22c0b47507956e707b78800cfd2db4
mferreirofr <mferreiro@iesjoandaustria.org>
2023-07-18 23:32:29 +0200

    remove unused destructured variables

--- packages/react-live/src/components/Live/LiveEditor.tsx
+++ packages/react-live/src/components/Live/LiveEditor.tsx
@@ -3,7 +3,7 @@ import LiveContext from "./LiveContext";
 import Editor, { CodeEditorProps as EditorProps } from "../Editor";
 
 export default function LiveEditor(props: Partial<EditorProps>) {
-  const { code, language, theme, disabled, onChange } = useContext(LiveContext);
+  const { code, onChange } = useContext(LiveContext);
 
   return <Editor initialValue={code} onChange={onChange} {...props} />;
 }
